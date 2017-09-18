52e9850ac8253a0bf9f2e838c25d3b2c513b4e5b
arunansu <arunansu@hotmail.com>
2017-09-17 21:18:19 -0400

    FEAT: Completed ASL Recognizer

--- AI/SignLanguageRecognizer/my_model_selectors.py
+++ AI/SignLanguageRecognizer/my_model_selectors.py
@@ -77,7 +77,19 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        bic_scores = []
+        try:
+            for n in self.n_components:
+                model = self.base_model(n)
+                log_l = model.score(self.X, self.lengths)
+                p = n ** 2 + 2 * n * model.n_features - 1
+                bic_score = -2 * log_l + p * math.log(n)
+                bic_scores.append(bic_score)
+        except Exception as e:
+            pass
+
+        states = self.n_components[np.argmax(bic_scores)] if bic_scores else self.n_constant
+        return self.base_model(states)
 
 
 class SelectorDIC(ModelSelector):
@@ -93,7 +105,23 @@ class SelectorDIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        dic_scores = []
+        logs_l = []
+        try:
+            for n_component in self.n_components:
+                model = self.base_model(n_component)
+                logs_l.append(model.score(self.X, self.lengths))
+            sum_logs_l = sum(logs_l)
+            m = len(self.n_components)
+            for log_l in logs_l:
+                # DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+                other_words_likelihood = (sum_logs_l - log_l) / (m - 1)
+                dic_scores.append(log_l - other_words_likelihood)
+        except Exception as e:
+            pass
+
+        states = self.n_components[np.argmax(dic_scores)] if dic_scores else self.n_constant
+        return self.base_model(states)
 
 
 class SelectorCV(ModelSelector):
@@ -105,4 +133,24 @@ class SelectorCV(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        mean_scores = []
+        # Save reference to 'KFold' in variable as shown in notebook
+        split_method = KFold()
+        try:
+            for n_component in self.n_components:
+                model = self.base_model(n_component)
+                # Fold and calculate model mean scores
+                fold_scores = []
+                for _, test_idx in split_method.split(self.sequences):
+                    # Get test sequences
+                    test_X, test_length = combine_sequences(test_idx, self.sequences)
+                    # Record each model score
+                    fold_scores.append(model.score(test_X, test_length))
+
+                # Compute mean of all fold scores
+                mean_scores.append(np.mean(fold_scores))
+        except Exception as e:
+            pass
+
+        states = self.n_components[np.argmax(mean_scores)] if mean_scores else self.n_constant
+        return self.base_model(states)
